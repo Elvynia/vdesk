b6c9567dded34292098daa4fa2ae52ce81d6aa94
elvynia <elvynia@gmail.com>
2017-07-15 12:20:57 +0200

    Deleted everything to use angular 4 CLI.

--- src/main-dev.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { RammModule } from './ramm/ramm.module';
-
-//enableProdMode();
-platformBrowserDynamic().bootstrapModule(RammModule).then((moduleRef) => {
-	moduleRef.instance.initialize('http://localhost:8080/spring');
-});
