999621822749cf8da7a69808c1fa4390ba3514ea
LV <elvynia@gmail.com>
2024-12-19 21:58:50 +0100

    Reset for nestjs workspace.

--- README.md
+++
@@ -1,2 +0,0 @@
-# RAMM
-Reckless Access Memory Map
--- src/main.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { RammModule } from './ramm/ramm.module';
-
-enableProdMode();
-platformBrowserDynamic().bootstrapModule(RammModule).then((moduleRef) => {
-	moduleRef.instance.initialize('https://ramm-backend.herokuapp.com/spring');
-});
