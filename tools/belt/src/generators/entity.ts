88ceb0071f1aaca0681f5c53ca665cbe114b9287
LV <elvynia@gmail.com>
2025-01-12 20:26:26 +0100

    Enhanced generator entity frontend service with getFields from multichoice prompt.

--- tools/belt/src/generators/entity.ts
+++ tools/belt/src/generators/entity.ts
@@ -3,9 +3,10 @@ import {
 	generateFiles,
 	Tree
 } from '@nx/devkit';
+import { select } from 'inquirer-select-pro';
 import * as path from 'path';
-import { EntityGeneratorSchema } from './schema';
 import { promptForEntity } from './prompt';
+import { EntityGeneratorSchema } from './schema';
 
 function makeExportUpdater(tree: Tree) {
 	return (target: string, folder: string, name: string, files: string[]) => {
@@ -25,7 +26,11 @@ export async function entityGenerator(
 	const exportUpdater = makeExportUpdater(tree);
 	options.clazz = options.name.charAt(0).toUpperCase() + options.name.slice(1);
 	options.fields = await promptForEntity();
-
+	options.fetchFields = await select({
+		message: 'select',
+		options: options.fields.map(({ name }) => ({ name, value: name })).concat([{ name: '_id', value: '_id' }]),
+		defaultValue: options.fields.map(({ name }) => name).concat(['_id'])
+	});
 	// Backend
 	generateFiles(tree, path.join(__dirname, 'backend'), `libs/${backlib}/src/lib/${options.name}`, options);
 	exportUpdater('libs/entity/src/index.ts', options.name, options.name, ['entity', 'module', 'resolver', 'service']);
@@ -42,7 +47,7 @@ export async function entityGenerator(
 	exportUpdater('libs/angular/src/index.ts', options.name + '/list', 'list', ['component']);
 
 	// Frontend app
-	generateFiles(tree, path.join(__dirname, 'frontend/app'), `apps/${frontapp}/src/lib/${options.name}`, options);
+	generateFiles(tree, path.join(__dirname, 'frontend/app'), `apps/${frontapp}/src/app/${options.name}`, options);
 
 	await formatFiles(tree);
 }
