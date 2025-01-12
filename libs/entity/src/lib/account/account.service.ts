6370db31f97bf976ffae3f85e766bc37c55e1a5c
LV <elvynia@gmail.com>
2025-01-12 19:53:54 +0100

    Enhanced account item component. Updated item/list components in entity generator.

--- libs/entity/src/lib/account/account.service.ts
+++ libs/entity/src/lib/account/account.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { EntityService } from '../entity.service';
 import { AccountCreate, AccountEntity } from './account.entity';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AccountService extends EntityService<AccountEntity, AccountCreate> {
@@ -10,6 +11,13 @@ export class AccountService extends EntityService<AccountEntity, AccountCreate> 
 		@InjectModel(AccountEntity.name) protected readonly model: Model<AccountEntity>
 	) {
 		super();
+	}
+
+	override create(editEntity: AccountCreate): Observable<AccountEntity> {
+		return super.create({
+			...editEntity,
+			creationDate: new Date().toISOString()
+		} as AccountEntity)
 	}
 
 	findByUsername(username: string) {
